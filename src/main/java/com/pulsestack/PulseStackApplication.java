d92982b6b3aeddb3b2136b838aad3356b500c70b
Jainil Jain <jainjainil27@gmail.com>
2025-04-27 00:55:43 +0530

    initial setup

---
+++ src/main/java/com/pulsestack/PulseStackApplication.java
@@ -0,0 +1,13 @@
+package com.pulsestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulsestack/PulseStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsestack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
