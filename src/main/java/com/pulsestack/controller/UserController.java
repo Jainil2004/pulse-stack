e9b3217ca2d41ec31eaddfd3e3d63430f6c8db28
Jainil Jain <jainjainil27@gmail.com>
2025-04-27 23:28:41 +0530

    improved userController

--- src/main/java/com/pulsestack/controller/UserController.java
+++ src/main/java/com/pulsestack/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pulsestack.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,12 @@ public class UserController {
     @GetMapping("/checkStatus")
     public String checkUserStatus() {
         return "this is a user only endpoint";
+    }
+
+    @GetMapping("/profile")
+    public String getUserProfile(Authentication authentication) {
+        String username = authentication.getName();
+        return "profile information for user: " + username;
+        
     }
 }
